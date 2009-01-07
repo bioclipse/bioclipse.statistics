1f1b60a886fd3256d4e4029d8cd288744467b430
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-01-07 15:55:53 +0000

    reverted r8084, except for files that caused conflicts
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@8147 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.statistics.jama/src/net/bioclipse/plugins/Bc_jamaPlugin.java
+++ plugins/net.bioclipse.statistics.jama/src/net/bioclipse/plugins/Bc_jamaPlugin.java
@@ -9,77 +9,96 @@
  *     Egon Willighagen - core API and implementation
  *******************************************************************************/
 package net.bioclipse.plugins;
+
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Properties;
+
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
+
 /**
  * Adds matrix power to Bioclipse.
  * 
  * @author egonw
  */
 public class Bc_jamaPlugin extends AbstractUIPlugin {
-        private final String PLUGIN_ID="net.bioclipse.statistics.jama";
-        private final String LOG_PROPERTIES_FILE="logger.properties";
-        //The shared instance.
-        private static Bc_jamaPlugin plugin;
-        /**
-         * The constructor.
-         */
-        public Bc_jamaPlugin() {
-                plugin = this;
-        }
-        /**
-         * This method is called upon plug-in activation
-         */
-        public void start(BundleContext context) throws Exception {
-                super.start(context);
-                // New logger with com.tools.logging
-                configureLogger();
-                System.out.println("Starting jama plugin...");
-        }
-        /**
-         * This method is called when the plug-in is stopped
-         */
-        public void stop(BundleContext context) throws Exception {
-                super.stop(context);
-                plugin = null;
-        }
-        /**
-         * Returns the shared instance.
-         */
-        public static Bc_jamaPlugin getDefault() {
-                return plugin;
-        }
-        /**
-         * Returns an image descriptor for the image file at the given
-         * plug-in relative path.
-         *
-         * @param path the path
-         * @return the image descriptor
-         */
-        public static ImageDescriptor getImageDescriptor(String path) {
-                return AbstractUIPlugin.imageDescriptorFromPlugin("net.bioclipse.statistics.jama", path);
-        }
-        private void configureLogger() {
-                try {
-                        URL url = Platform.getBundle(PLUGIN_ID).getEntry("/" + LOG_PROPERTIES_FILE);
-                        InputStream propertiesInputStream = url.openStream();
-                        if (propertiesInputStream != null) {
-                                Properties props = new Properties();
-                                props.load(propertiesInputStream);
-                                propertiesInputStream.close();
-                        }	
-                } 
-                catch (Exception e) {
-                        String message = "Error while initializing log properties." + 
-                        e.getMessage();
-                        System.err.println(message);
-                        throw new RuntimeException(
-                                        "Error while initializing log properties.",e);
-                }         
-        }	
+	
+	private final String PLUGIN_ID="net.bioclipse.statistics.jama";
+	private final String LOG_PROPERTIES_FILE="logger.properties";
+	
+	//The shared instance.
+	private static Bc_jamaPlugin plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public Bc_jamaPlugin() {
+		plugin = this;
+	}
+	
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		
+		// New logger with com.tools.logging
+		configureLogger();
+		
+		System.out.println("Starting jama plugin...");
+	}
+	
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+	
+	/**
+	 * Returns the shared instance.
+	 */
+	public static Bc_jamaPlugin getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return AbstractUIPlugin.imageDescriptorFromPlugin("net.bioclipse.statistics.jama", path);
+	}
+	
+	private void configureLogger() {
+
+		try {
+			URL url = Platform.getBundle(PLUGIN_ID).getEntry("/" + LOG_PROPERTIES_FILE);
+			
+			InputStream propertiesInputStream = url.openStream();
+			
+			
+			if (propertiesInputStream != null) {
+				Properties props = new Properties();
+				props.load(propertiesInputStream);
+				propertiesInputStream.close();
+			}	
+		} 
+		catch (Exception e) {
+			String message = "Error while initializing log properties." + 
+			e.getMessage();
+			System.err.println(message);
+			throw new RuntimeException(
+					"Error while initializing log properties.",e);
+		}         
+	}	
+	
+
 }
