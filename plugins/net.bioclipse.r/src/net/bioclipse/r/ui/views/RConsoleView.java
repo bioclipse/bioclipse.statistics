8b7787298f23fa4c8d5095b9b4b1469735fbebd2
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-12 16:03:07 +0000

    added R console
    it doesn't do anything yet, but it's a console.
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9280 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.r/src/net/bioclipse/r/ui/views/RConsoleView.java
@@ -0,0 +1,18 @@
+/*******************************************************************************
+ *Copyright (c) 2008 The Bioclipse Team and others.
+ *All rights reserved. This program and the accompanying materials
+ *are made available under the terms of the Eclipse Public License v1.0
+ *which accompanies this distribution, and is available at
+ *http://www.eclipse.org/legal/epl-v10.html
+ *
+ *******************************************************************************/
+package net.bioclipse.r.ui.views;
+
+import net.bioclipse.scripting.ui.views.ScriptingConsoleView;
+
+public class RConsoleView extends ScriptingConsoleView {
+    @Override
+    protected String executeCommand( String command ) {
+        return null;
+    }
+}
