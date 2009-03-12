36b8b3eb8600ce11f808d69a113c6c0db26d141f
carl_masak <carl_masak@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-12 16:03:38 +0000

    added code to interface with R
    This should make the R console come to life. It doesn't. Need to find out why.
    
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9281 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.r/src/net/bioclipse/r/ui/views/RConsoleView.java
+++ plugins/net.bioclipse.r/src/net/bioclipse/r/ui/views/RConsoleView.java
@@ -8,11 +8,36 @@
  *******************************************************************************/
 package net.bioclipse.r.ui.views;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.bioclipse.r.NoRException;
+import net.bioclipse.r.TextR;
+import net.bioclipse.scripting.OutputProvider;
 import net.bioclipse.scripting.ui.views.ScriptingConsoleView;
 
 public class RConsoleView extends ScriptingConsoleView {
+    TextR env;
+
+    public RConsoleView() throws NoRException {
+        env = new TextR(new OutputProvider() {
+            @Override
+            public void output( String s ) {
+                printMessage(s);
+            }
+        });
+    }
+
     @Override
     protected String executeCommand( String command ) {
+        ArrayList<Object> l = new ArrayList<Object>();
+        l.add( command );
+        try {
+            env.eval( l );
+        } catch ( IOException e ) {
+            printMessage(e.getMessage());
+        }
         return null;
     }
 }
