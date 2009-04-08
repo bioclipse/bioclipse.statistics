7520c027e6c0e218d33305d9b59ab6761d9ed61d
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-04-08 14:43:08 +0000

    Renamed Bc_Jama to Activator and adapted to 2.0, e.g. removed bc1 logging.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9708 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.statistics.jama/src/net/bioclipse/plugins/Activator.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2009 Bioclipse Project
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Egon Willighagen - core API and implementation
+ *     Ola Spjuth - updated for Bioclipse 2.0
+ *******************************************************************************/
+package net.bioclipse.plugins;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * Adds matrix power to Bioclipse.
+ * 
+ * @author egonw, ola
+ */
+public class Activator extends AbstractUIPlugin {
+	
+	public static final String PLUGIN_ID="net.bioclipse.statistics.jama";
+	
+	//The shared instance.
+	private static Activator plugin;
+	
+	/**
+	 * The constructor.
+	 */
+	public Activator() {
+		plugin = this;
+	}
+	
+	/**
+	 * This method is called upon plug-in activation
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+	}
+	
+	/**
+	 * This method is called when the plug-in is stopped
+	 */
+	public void stop(BundleContext context) throws Exception {
+		super.stop(context);
+		plugin = null;
+	}
+	
+	/**
+	 * Returns the shared instance.
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+	
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path.
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+	    return AbstractUIPlugin.imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+}
