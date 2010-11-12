468eae2cbc7125fc66abd09c271ff5b042bfc8c1
Carl Masak <cmasak@gmail.com>
2010-11-12 16:17:42 +0100

    replaced shell process with JRI process
    This makes for a more robust connection with R, and hopefully provides a
    better way to communicate back and forth between R and Bioclipse,
    long-term. The console output still needs some fixing-up, and right now
    Bioclipse dies unless provided with the VM argument
    
     -Djava.library.path="/Library/Frameworks/R.framework/Resources/library/rJava/jri:"
    
    (Settable under the tab "Arguments" in Run Configurations.)

--- plugins/net.bioclipse.r/src/net/bioclipse/r/ui/views/RConsoleView.java
+++ plugins/net.bioclipse.r/src/net/bioclipse/r/ui/views/RConsoleView.java
@@ -8,113 +8,20 @@
  *******************************************************************************/
 package net.bioclipse.r.ui.views;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import net.bioclipse.r.NoRException;
-import net.bioclipse.r.TextR;
-import net.bioclipse.scripting.Hook;
-import net.bioclipse.scripting.JsAction;
-import net.bioclipse.scripting.JsThread;
-import net.bioclipse.scripting.OutputProvider;
 import net.bioclipse.scripting.ui.views.ScriptingConsoleView;
 
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.widgets.Display;
+import org.rosuda.JRI.Rengine;
 
 public class RConsoleView extends ScriptingConsoleView {
-    TextR env;
+    private Rengine re;
 
-    private static JsThread jsThread
-        = net.bioclipse.scripting.Activator.getDefault().JS_THREAD;
-    private static Matcher jsBacktickMatcher;
-
-    static {
-        String noBackTick
-          = "("                                   // capture into group:
-              + "(?:"                               // either...
-                + "[^`\'\"]+"                          // no backticks or quotes
-                + "|"                                  // ...or...
-                + "(?:\"(?:[^\"\\\\]|(?:\\\\.))*\")"   // a double quoted string
-                + "|"                                  // ...or...
-                + "(?:\'(?:[^\'\\\\]|(?:\\\\.))*\')"   // a single quoted string
-              + ")*+"                               // zero or more of the above
-            + ")";
-
-        jsBacktickMatcher
-          = Pattern.compile( noBackTick + "`" + noBackTick + "`" ).matcher("");
-    }
-
-    public RConsoleView() throws NoRException {
-        env = new TextR(new OutputProvider() {
-            @Override
-            public void output( final String s ) {
-                Display.getDefault().asyncExec( new Runnable() {
-                    public void run() {
-                        printMessage(s);
-                    }
-                });
-            }
-        });
-        String loc=Platform.getLocation().toOSString();
-        if (loc!=null)
-            executeCommand( "setwd(\""+ loc +"\")");
+    public RConsoleView() {
+        re = new Rengine(new String[] {"--vanilla"}, false, null);
     }
 
     @Override
     protected String executeCommand( String command ) {
-        List<Object> al;
-        al = interpolateJsVariables(command);
-
-        try {
-            env.eval( al );
-        } catch ( IOException e ) {
-            printMessage(e.getMessage());
-        }
-
-        return null;
-    }
-
-    private List<Object> interpolateJsVariables(String command) {
-
-        List<Object> al = new ArrayList<Object>();
-        jsBacktickMatcher.reset(command);
-
-        int e = 0;
-        while(jsBacktickMatcher.find()){
-
-            al.add(jsBacktickMatcher.group(1));
-            final Object[] value = new Object[] { null };
-            jsThread.enqueue(new JsAction(jsBacktickMatcher.group(2),
-                    new Hook() {
-                        public void run(Object result) {
-                            synchronized(value) {
-                              value[0] = result;
-                              value.notify();
-                            }
-                        }
-            }));
-
-            // This sucks. Seriously.
-            synchronized(value) {
-                while (value[0] == null) {
-                    try {
-                        value.wait();
-                    } catch (InterruptedException e1) {
-                        return new ArrayList<Object>();
-                    }
-                }
-            }
-
-            al.add(value[0]);
-            e = jsBacktickMatcher.end();
-        }
-        al.add(command.substring(e));
-
-        return al;
+        return re.eval(command).toString();
     }
 
     protected void waitUntilCommandFinished() {
