86c9a8c2d2e9e474c1361643142c44b3635e5202
Christian Ander <christian@eximius.se>
2011-02-24 16:00:08 +0100

    Added a logger and check if R_HOME is set. Make sure to include previous Commit to update the MANIFEST.MF.

--- plugins/net.bioclipse.r.ui/src/net/bioclipse/r/ui/views/RConsoleView.java
+++ plugins/net.bioclipse.r.ui/src/net/bioclipse/r/ui/views/RConsoleView.java
@@ -11,26 +11,37 @@ package net.bioclipse.r.ui.views;
 import net.bioclipse.r.Rengine;
 import net.bioclipse.scripting.ui.views.ScriptingConsoleView;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class RConsoleView extends ScriptingConsoleView {
 
-	private Rengine re;
+    private Rengine re;
+    final Logger logger = LoggerFactory.getLogger(RConsoleView.class);
 
     public RConsoleView() {
         BioclipseRLoopCallback lc = new BioclipseRLoopCallback(this);
+        logger.info("RConsole: Starting R..");
+        logger.debug("RConsole: R_HOME =" + System.getenv("R_HOME"));
+        logger.debug("RConsole: java.library.path =" + System.getProperty("java.library.path"));
+
         re = new Rengine(new String[] {"--vanilla"}, false, lc);
     	  if (!org.rosuda.JRI.Rengine.versionCheck()) {
-    	      System.err.println("Rengine: Version mismatch, java files don't match library version.");
+    	      logger.error("Rengine: Version mismatch, java files don't match library version.");
     	  }
-    	  lc.doWrite = true;   // enable verbose output from R. 
+    	  lc.doWrite = true;   // enable output to R-consoleView. 
     }
 
     @Override
     protected String executeCommand( String command ) {
         echoCommand(command);
-        System.out.println("R cmd: " + command);
+        logger.debug("R cmd: " + command);
         String returnVal;
 
-        if (command.equals("q()") || command.equals("quit()") ) { returnVal = "Cannot quit R from here"; }
+        if (command.equals("q()") || command.equals("quit()") )
+            returnVal = "Cannot quit R from here";
+        else if (System.getenv("R_HOME") == null)   // check if R_HOME is set to avoid crash.
+            returnVal = "R_HOME not set."; 
         else {
             try { returnVal = re.evalCommand(command); }
             catch (Throwable error) {
@@ -38,7 +49,7 @@ public class RConsoleView extends ScriptingConsoleView {
     	      return "Error: " + error.getMessage();
             }
         }
-        System.out.println(" -> " + returnVal);
+        logger.debug(" -> " + returnVal);
         printMessage(returnVal);
         return returnVal;
     }
