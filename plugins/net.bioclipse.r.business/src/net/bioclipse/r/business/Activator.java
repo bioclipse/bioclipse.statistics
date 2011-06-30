ea90a2e1c272ef61a6b4f23a4f55cd553645f1ef
Christian Ander <christian@eximius.se>
2011-06-30 09:43:07 +0200

    Found another way to handle time-outs

--- plugins/net.bioclipse.r.business/src/net/bioclipse/r/business/Activator.java
+++ plugins/net.bioclipse.r.business/src/net/bioclipse/r/business/Activator.java
@@ -65,7 +65,7 @@ public class Activator extends Plugin {
         IRBusinessManager manager = null;
         try {
             manager = (IRBusinessManager)
-                      javaFinderTracker.waitForService(1500*10); // 1000*10 is to small.
+                      javaFinderTracker.waitForService(1000*10);
         }
         catch (InterruptedException e) {
             throw new IllegalStateException(
